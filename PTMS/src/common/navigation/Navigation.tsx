5a5056a61594b0d918e82227b23e13936bf74ff1
MirekSobota <miroslaw.sobota@gmail.com>
2023-10-30 15:29:50 +0100

    3-PTMS- created Navigation component

--- PTMS/src/common/navigation/Navigation.tsx
+++ PTMS/src/common/navigation/Navigation.tsx
@@ -1,18 +1,27 @@
 import { BottomNav } from "./BottomNav";
 import Logo from "../../assets/logoB&W.svg";
 import Box from "@mui/material/Box";
+import  Grid  from "@mui/material/Grid";
 
 export const Navigation = () => {
     
     return (
-        <Box >
-            <BottomNav />
-            <Box
-                component="img"
-                src={Logo}
-                alt="Logo"
-            />
-        </Box>
+        <Grid container spacing={2} 
+        sx={{
+            background: '#dfdfdf',
+            width: "100%", 
+            justifyContent: "space-around",
+            marginLeft: "auto",            
+          
+          }}
+        >
+            <Grid item>
+                <Box component="img" src={Logo} alt="Logo" />
+            </Grid>
+            <Grid item>
+                <BottomNav />
+            </Grid>
+        </Grid>
     );
 };
 
