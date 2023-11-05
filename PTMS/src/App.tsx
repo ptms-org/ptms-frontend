a825f929742af876fd38b8d87722d678d1b795cf
MirekSobota <miroslaw.sobota@gmail.com>
2023-11-05 20:49:28 +0100

    3-PTMS-implemented router

--- PTMS/src/App.tsx
+++ PTMS/src/App.tsx
@@ -1,12 +1,23 @@
-import './App.css'
+import { Route, createBrowserRouter,createRoutesFromElements, RouterProvider } from "react-router-dom";
+
+import "./App.css";
+import { Navigation } from "./common/navigation/Navigation";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route path="/" element={ <Navigation />}>
+        <Route path="/Home" element={<></>} />
+        <Route path="/CreateProject" element={<></>} />
+        <Route path="/User" element={<></>} />
+      </Route>
+  ));
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+      <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
+
