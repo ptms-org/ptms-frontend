64e99ef0bf0c09b01d0fbd2e591364532b4c7d5d
MirekSobota <miroslaw.sobota@gmail.com>
2023-11-05 20:53:15 +0100

    3-PTMS-added css

--- PTMS/src/common/navigation/Navigation.tsx
+++ PTMS/src/common/navigation/Navigation.tsx
@@ -1,27 +1,49 @@
 import { BottomNav } from "./BottomNav";
 import Logo from "../../assets/logoB&W.svg";
 import Box from "@mui/material/Box";
-import  Grid  from "@mui/material/Grid";
+import Grid from "@mui/material/Grid";
+import { Outlet } from "react-router-dom";
+import { useResponsive } from "../mediaQuery";
 
 export const Navigation = () => {
-    
-    return (
-        <Grid container spacing={2} 
+  const { media630, media580, media420 } = useResponsive();
+  return (
+    <>
+      <Grid
+        container
+        spacing={0}
         sx={{
-            background: '#dfdfdf',
-            width: "100%", 
-            justifyContent: "space-around",
-            marginLeft: "auto",            
-          
+          background: "#dfdfdf",
+          width: "100%",
+          justifyContent: media420 ? "center" : "space-between",
+          flexWrap: "wrap",
+          marginLeft: "auto",
+        }}
+      >
+        <Grid item>
+          <Box
+            sx={{
+              width: media630 ? 120 : "auto",
+              padding: media580 ? "10px" : 1,
+              paddingBottom: 0,
+            }}
+            component="img"
+            src={Logo}
+            alt="Logo"
+          />
+        </Grid>
+        <Grid
+          item
+          sx={{
+            padding: 0,
           }}
         >
-            <Grid item>
-                <Box component="img" src={Logo} alt="Logo" />
-            </Grid>
-            <Grid item>
-                <BottomNav />
-            </Grid>
+          <BottomNav />
         </Grid>
-    );
+      </Grid>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 };
-
