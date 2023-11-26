e3a7018fcf7e345146a3e8ffc013dc54c573995b
MirekSobota <miroslaw.sobota@gmail.com>
2023-11-26 21:49:19 +0100

    6-PTMS-temp. users list added

---
+++ PTMS/src/temporaryFiles/users.ts
@@ -0,0 +1,100 @@
+export const users = [
+  {
+    id: 1,
+    name: "Borys Jelcyn",
+    email: "jelcyn@gmail.com",
+    img: "https://images.unsplash.com/photo-1682686580224-cd46ea1a6950?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    name: "Robert Kowalski",
+    email: "kowalski@gmail.com",
+    img: "https://images.unsplash.com/photo-1700540291181-2d7be661477e?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    name: "Maria Jakubowska",
+    email: "jakubowska@gmail.com",
+    img: "https://images.unsplash.com/photo-1700493624674-ba1d0ec73595?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    email: "nowak@gmail.com",
+    name: "Piotre Nowak",
+    img: "https://plus.unsplash.com/premium_photo-1695203199983-d061c85f0bdd?q=80&w=2002&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 5,
+    name: "Borys Jelcyn",
+    email: "jelcyn@gmail.com",
+    img: "https://plus.unsplash.com/premium_photo-1680796736382-1dc74d011b6b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 6,
+    name: "Robert Kowalski",
+    email: "kowalski@gmail.com",
+    img: "https://images.unsplash.com/photo-1683900694874-6885a9a0523e?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 7,
+    name: "Maria Jakubowska",
+    email: "jakubowska@gmail.com",
+    img: "https://plus.unsplash.com/premium_photo-1692193552327-3458ef3817c0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 8,
+    email: "nowak@gmail.com",
+    name: "Piotre Nowak",
+    img: "https://plus.unsplash.com/premium_photo-1695203199983-d061c85f0bdd?q=80&w=2002&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  }, {
+    id: 9,
+    name: "Borys Jelcyn",
+    email: "jelcyn@gmail.com",
+    img: "https://images.unsplash.com/photo-1699730162391-142c6c174f26?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 10,
+    name: "Robert Kowalski",
+    email: "kowalski@gmail.com",
+    img: "https://images.unsplash.com/photo-1700540291181-2d7be661477e?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 11,
+    name: "Maria Jakubowska",
+    email: "jakubowska@gmail.com",
+    img: "https://images.unsplash.com/photo-1700493624674-ba1d0ec73595?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 12,
+    email: "nowak@gmail.com",
+    name: "Piotre Nowak",
+    img: "https://plus.unsplash.com/premium_photo-1695203199983-d061c85f0bdd?q=80&w=2002&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 13,
+    name: "Borys Jelcyn",
+    email: "jelcyn@gmail.com",
+    img: "https://images.unsplash.com/photo-1682686580224-cd46ea1a6950?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 14,
+    name: "Robert Kowalski",
+    email: "kowalski@gmail.com",
+    img: "https://images.unsplash.com/photo-1700540291181-2d7be661477e?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 15,
+    name: "Maria Jakubowska",
+    email: "jakubowska@gmail.com",
+    img: "https://images.unsplash.com/photo-1700493624674-ba1d0ec73595?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 16,
+    email: "nowak@gmail.com",
+    name: "Piotre Nowak",
+    img: "https://plus.unsplash.com/premium_photo-1695203199983-d061c85f0bdd?q=80&w=2002&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+ 
+];
+
+ 
